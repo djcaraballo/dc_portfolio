21180ee82a958a57332f56542c9051d3831f993f
Dina Caraballo <dina.caraballo@flatironschool.com>
2019-05-08 15:11:18 -0600

    Add nav links and icons to nav bar

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -7,6 +7,10 @@ const Contact = () => {
     <div>
       <PageNav />
       <h1>Contact</h1>
+      <i className="fab fa-twitter-square">@DinaCaraballo</i>
+      <i className="fab fa-linkedin">LinkedIn.com/in/Dina-Caraballo</i>
+      <i className="fab fa-github">Github.com/djcaraballo</i>
+      <i className="fas fa-envelope-square">DCaraballo26@gmail.com</i>
       <h3>Coming Soon!</h3>
     </div>
   )
