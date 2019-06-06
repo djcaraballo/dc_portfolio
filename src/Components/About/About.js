755d11dc854dad733d24d8bf1ff398120fef593c
Dina Caraballo <dina.caraballo@flatironschool.com>
2019-06-05 21:23:29 -0600

    Minor refactor of several things--nothing broken!

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -7,7 +7,13 @@ const About = () => {
     <div>
       <PageNav />
       <h1>About</h1>
-      <h3>Coming Soon!</h3>
+      <div className="about-text">
+        <p>I'm a software engineer with an eye for detail, a passion for writing clean code and developing efficient user experiences and interfaces. I thrive in dynamic, collaborative environments where I can transform challenging problems with tight constraints, into elegant and tested solutions through continuous agile processes.</p>
+
+        <p>Front end javascript libraries I work with revolve around popular implementations of the observer design pattern: Creating views through composable React components and managing state with Redux in response to actions or side effects e.g. consuming RESTful APIs or delaying code execution. In parallel with client-side development, I have been learning and working with Node, Express, Knex, PostgreSQL, GraphQL, Ruby on Rails and RESTful API creation to fill out my server-side experience.</p>
+
+        <p>As a software engineer, I desire to positively impact the future in a meaningful and widespread way. I have a growing curiosity of 21st century educational tools and tech, designed by forward-thinking, creative professionals; as well as cryptocurrency and biotechnology. </p>
+      </div>
     </div>
   )
 }
