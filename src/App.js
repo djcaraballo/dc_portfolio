08801486949cadea16c6c8bb05235b3fb636ced2
Dina <dcaraballo26@gmail.com>
2019-06-26 14:16:35 -0600

    Add svg components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
 
 import './App.css'
 import About from './Components/About/About'
--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Landing.css'
 import LandingNav from '../LandingNav/LandingNav'
+import ScriptLogo from '../Svg/ScriptLogo'
+import SansLogo from '../Svg/SansLogo'
 
 const Landing = () => {
   return (
     <div>
-      <h1 className="my-name">Dina Caraballo</h1>
+      // <h1 className="my-name">Dina Caraballo</h1>
+      <SansLogo />
+      <ScriptLogo />
       <h3 className="my-title">SOFTWARE ENGINEER</h3>
       <LandingNav />
     </div>
